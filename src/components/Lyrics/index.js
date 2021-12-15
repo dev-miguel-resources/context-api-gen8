95819ff9d11bc213ca76055aee56b5f02d91f943
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-15 20:04:36 -0300

    defining context-api with app routes

---
+++ src/components/Lyrics/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lyrics = () => {
+  return <div><p>Hello am Lyrics!!!</p></div>;
+};
+
+export default Lyrics;
---
+++ src/contexts/SongsContext.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SongsContextProvider = () => {
+  return <div><p>Hello am SongsContext!!!</p></div>;
+};
+
+export default SongsContextProvider;
