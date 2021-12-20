fbceae78ef815740b93e3d183436c8de76d58b23
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-20 20:11:37 -0300

    defining songs page with context

---
+++ src/components/Common/ProgressBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProgressBar = () => {
+  return (
+    <div>
+      <p>Hello am ProgressBar!!</p>
+    </div>
+  );
+};
+
+export default ProgressBar;
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 const cors_anywhere = "https://cors-anywhere.herokuapp.com/";
 const base_url = "https://api.musixmatch.com/ws/1.1/";
-const chart_tracks_get="chart.tracks.get?chart_name=top&page=1&page_size=10&f_has_lyrics=1";
-const track_search="track.search?q_track";
+const chart_tracks_get = "chart.tracks.get?chart_name=top&page=1&page_size=10&f_has_lyrics=1";
+const track_search = "track.search?q_track=";
 const track_search_params="&page=1&page_size=10&f_has_lyrics=1&s_track_rating=desc";
 const tracks_lyrics_get="track.lyrics.get?commontrack_id=";
 const track_get="track.get?commontrack_id=";
@@ -11,3 +11,4 @@ export const chartTracksGet = () => `${ cors_anywhere }${base_url}${ chart_track
 export const trackSearch = q_track => `${ cors_anywhere }${base_url}${ track_search }${ q_track  }${ track_search_params }${ api_key }`;
 export const trackLyricsGet = commontrack_id => `${ cors_anywhere }${base_url}${ tracks_lyrics_get }${ commontrack_id }${ api_key }`;
 export const trackGet = commontrack_id => `${ cors_anywhere }${ base_url }${ track_get }${ commontrack_id }${ api_key }`;
+
